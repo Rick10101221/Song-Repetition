a67c8be21904d4c5d14368311989df049a3a58e6
Rickesh Khilnani <rikhilna@ucsd.edu>
2020-11-09 00:02:20 -0800

    finished lyric retrieval

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Library imports for dataframe, json formatting
 import pandas as pd
+import numpy as np
 import time
 import requests
 import json
+import lyricsgenius
 from lyrics_extractor import SongLyrics
 
 # Local file imports
@@ -12,55 +14,38 @@ from config import *
 
 def main(): 
   df = pd.read_csv('song-data-2000s.csv')
-  extract_lyrics = SongLyrics(GCS_API_KEY, GCS_ENGINE_ID)
+  # extract_lyrics = SongLyrics(GCS_API_KEY, GCS_ENGINE_ID)
+  genius = lyricsgenius.Genius(client_access_token)
 
-
-  # ================ COMMENT OUT THE FOLLOWING  ================
-  # ================ IF DATA HAS BEEN RETREIVED ================
   # Query and Make Calls
-  queries = 3
-  lyrics = getData(extract_lyrics, queries)
-  # Write to external file to prevent data loss
-  writeLyrics(lyrics)
-  # ============================================================
-  # ============================================================
+  queries = 1
+  lyrics = getData(genius, df, queries)
 
-
+  rows, columns = df.shape
+  if len(lyrics) != rows:
+    numMissingRows = rows - len(lyrics)
+    lyrics += [np.NaN] * numMissingRows
+    print(len(lyrics), lyrics)
   # Adds new column with lyrics in the dataframe  
   df['lyrics'] = lyrics
-  printData(df)
+  df.to_csv('lyrics.csv')
 
 
-def writeLyrics(lyrics):
-  with open('lyrics.csv', 'w') as file:
-    for lyric in lyrics:
-      file.write(f'{lyric}||||||||')
-
-
-def getData(extract_lyrics, queries):
+def getData(genius, df, queries):
   lyrics = []
   for i, item in df.iterrows():
-    # Breaks if we've hit the query limit defined above
-    if i > queries:
-      break
-
-    # title, artist, reached_number_one = item
-
+    # if i == queries:
+    #   break
+    title, artist, reached_number_one = item
     # Keeps track of calls
     print(i)
-
-    lyric = extract_lyrics.get_lyrics(title)['lyrics']
-    if i < 5:
-      print(lyric)
+    try:
+      song = genius.search_song(title, artist)
+      lyric = song.lyrics
+    except:
+      lyric = np.NaN
     lyrics.append(lyric)
   return lyrics
-
-
-def printData(df):
-  for i, item in df.iterrows():
-    title, artist, reached_number_one, lyrics = item
-    print(title, artist, lyrics[:100])
-
 
 
 main()
